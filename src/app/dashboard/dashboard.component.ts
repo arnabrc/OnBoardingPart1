9bcad190bcfbf1648bda4529aaa9819e6ff6cc7e
Arnab <arnabroychowdhury6@gmail.com>
2020-08-17 16:50:11 +0530

    Commit003

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  date: any;
 
-  constructor() { }
+  constructor(public router: Router, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getTimestamp();
+  }
+
+  getTimestamp() {
+    return this.date = new Date();
+  }
+
+  checkUserList() {
+    this.router.navigate(['/userlist']);
   }
 
 }
