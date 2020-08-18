1a91862eb3e6556839fc76542b8a6510a982055a
Arnab <arnabroychowdhury6@gmail.com>
2020-08-18 11:32:25 +0530

    Commit004

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-userlist',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userlist.component.css']
 })
 export class UserlistComponent implements OnInit {
+  users: [any];
+  closebtns: any;
+  closeBtns: any;
+  editBtns: any;
+  imageShow: boolean;
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.getUsers().subscribe(data => {
+      this.users = data.sort((n1, n2) => n1.id > n2.id);
+    }, (err) => {
+      this.handleError(err);
+    });
+    this.closeBtns = document.getElementsByClassName('close');
+    this.editBtns = document.getElementsByClassName('edit');
+  }
+
+  handleError(err: HttpErrorResponse) {
+    if (err.error instanceof ErrorEvent) {
+      console.error('Error Event');
+    } else {
+      console.log(`error status : ${err.status} ${err.statusText}`);
+    }
+    return throwError(err);
+  }
+
+  userSelect() {
+    this.imageShow = !this.imageShow;
+  }
+
+  editUser(id: any) {
+    window.alert('Edit: ' + id);
+  }
+
+  deleteUser(id: any) {
+    this.users.splice(id, 1);
   }
 
 }
